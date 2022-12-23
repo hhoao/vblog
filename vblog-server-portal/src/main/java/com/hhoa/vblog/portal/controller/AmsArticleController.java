a293c5627a8efbb446980b5dcaf043037ccde4db
hhoao <haunghao@foxmail.com>
2022-12-23 10:52:52 +0800

    ci: Force deployment

--- vblog-server-portal/src/main/java/com/hhoa/vblog/portal/controller/AmsArticleController.java
+++ vblog-server-portal/src/main/java/com/hhoa/vblog/portal/controller/AmsArticleController.java
@@ -8,15 +8,11 @@ import com.hhoa.vblog.portal.service.AmsArticleService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.Null;
 import java.util.List;
 
 /**
